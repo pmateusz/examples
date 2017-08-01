a2d1716ad0ee0b26e27d8bf2e1030343d203051c
pmateusz <pmateusz@users.noreply.github.com>
2017-08-01 10:11:23 +0100

    Update third-party-install.sh
    Fix popd from empty stack

--- build_deb_package/shared/third-party-install.sh
+++ build_deb_package/shared/third-party-install.sh
@@ -16,32 +16,26 @@ mkdir -p ~/dev && pushd dev
 if [ ! -d glpk-4.35 ]; then
     wget http://ftp.gnu.org/gnu/glpk/glpk-4.35.tar.gz --quiet -Oglpk-4.35.tar.gz \
     && tar -xzf glpk-4.35.tar.gz \
-    && rm glpk-4.35.tar.gz
-    
-    pushd glpk-4.35
-    ./configure --with-pic && make && sudo make install
-    popd
+    && rm glpk-4.35.tar.gz \
+    && cd glpk-4.35 \
+    && ./configure --with-pic && make && sudo make install
 fi
 
 if [ ! -d gurobi751 ]; then
     wget http://packages.gurobi.com/7.5/gurobi7.5.1_linux64.tar.gz --quiet -Ogurobi7.5.1.tar.gz \
     && tar -xzf gurobi7.5.1.tar.gz \
     && rm gurobi7.5.1.tar.gz \
-    && sudo /bin/cp -rf gurobi751 /opt
-    
-    pushd /opt/gurobi751/lib
-    sudo ln -s libgurobi75.so libgurobi.so
-    popd
+    && sudo /bin/cp -rf gurobi751 /opt \
+    && cd /opt/gurobi751/lib \
+    && sudo ln -s libgurobi75.so libgurobi.so
 fi 
 
 if [ ! -d cmake-3.8.2 ]; then
     wget https://cmake.org/files/v3.8/cmake-3.8.2.tar.gz --quiet -Ocmake-3.8.2.tar.gz \
     && tar -xzf cmake-3.8.2.tar.gz \
-    && rm cmake-3.8.2.tar.gz
-    
-    pushd cmake-3.8.2
-    ./bootstrap --no-qt-gui --system-curl && make && sudo make install
-    popd
+    && rm cmake-3.8.2.tar.gz \
+    && cd cmake-3.8.2 \
+    && ./bootstrap --no-qt-gui --system-curl && make && sudo make install
 fi
 
 sudo ldconfig --verbose
